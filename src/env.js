9250c444a011c3f0b8b999b536975e80707d9676
MrCheater <slayerx@narod.ru>
2017-04-15 18:22:21 +0300

    update

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 process.env.MINI_CI_SECRET = process.env.MINI_CI_SECRET || 'MINI_CI_SECRET'
 process.env.MINI_CI_PORT = process.env.MINI_CI_PORT || '9876'
-process.env.MINI_CI_HOST = process.env.MINI_CI_HOST || 'localhost'
+process.env.MINI_CI_HOST = process.env.MINI_CI_HOST || '0.0.0.0'
 process.env.MINI_CI_PROTOCOL = process.env.MINI_CI_PROTOCOL || 'http'
 
